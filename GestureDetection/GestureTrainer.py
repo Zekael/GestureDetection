a2cc699639ec6e8a58a538e0e65b5c626f24de28
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-06-22 08:09:04 +0200

    inprogess

--- GestureDetection/GestureTrainer.py
+++ GestureDetection/GestureTrainer.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import tensorboard
 
-class handGestureTrainer():
+class ModelTrainer():
 
     def __init__(self,input_shape=(65,)):
         self.model = keras.Sequential()
@@ -17,13 +17,20 @@ class handGestureTrainer():
         self.model.add(layers.Dense(4,activation="softmax"))
 
 
-    def train(self,training_data,testing_data):
+    def fit(self,training_data,testing_data,Epochs=30,batch_size=10):
+        print("Training_data shape: "+str(training_data.shape))
+        print("Testing_data shape: "+str(testing_data.shape))
         self.model.compile(optimizer='adam', loss=tf.keras.losses.CategoricalCrossentropy())
-        self.model.fit(training_data,testing_data)
+        tensorboard_callbacks = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        self.model.fit(training_data,testing_data,callbacks=[tensorboard_callbacks])
 
 
+    def evaluate(self,validation_x,validation_y):
+        _,accuracy = self.model.evaluate(validation_x,validation_y)
 
 
 
 if __name__ == "__main__":
     trainer = handGestureTrainer()
+
+    data = pd.read_csv()
